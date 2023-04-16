2f76b4f72cae76547d82a220aaa98597f1ec3c89
ARRakshitha <rakshithaar878@gmail.com>
2023-04-16 21:32:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { FoodListComponent } from './food-list/food-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    FoodListComponent
   ],
   imports: [
     BrowserModule,
