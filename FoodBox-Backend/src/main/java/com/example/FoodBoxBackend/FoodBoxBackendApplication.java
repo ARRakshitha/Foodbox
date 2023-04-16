dec6389fd7f274f3d23bd1787654c223a5c089c4
ARRakshitha <rakshithaar878@gmail.com>
2023-04-16 21:37:50 +0530

    First commit

---
+++ FoodBox-Backend/src/main/java/com/example/FoodBoxBackend/FoodBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodBoxBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBoxBackendApplication.class, args);
+	}
+
+}
